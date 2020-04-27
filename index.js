f897a2fa64749ec5fc9eeabc734d6ec01333b520
Alb The Wizard <alberto98fx@gmail.com>
2020-04-27 16:52:58 +0200

    Test

--- index.js
+++ index.js
@@ -1,20 +1,37 @@
-const child_process = require('child_process');
-let out;
+const test = run => {
 
-out = child_process.spawnSync('true');
-console.log('status: ' + out.status);
-console.log('stdout: ' + out.stdout.toString('utf8'));
-console.log('stderr: ' + out.stderr.toString('utf8'));
-console.log();
+    const child_process = require('child_process');
+    let out;
+    
+    out = child_process.spawnSync('true');
+    console.log('status: ' + out.status);
+    console.log('stdout: ' + out.stdout.toString('utf8'));
+    console.log('stderr: ' + out.stderr.toString('utf8'));
+    console.log();
+    
+    out = child_process.spawnSync('false');
+    console.log('status: ' + out.status);
+    console.log('stdout: ' + out.stdout.toString('utf8'));
+    console.log('stderr: ' + out.stderr.toString('utf8'));
+    console.log();
+    
+    out = child_process.spawnSync('ls', ['-lhatr']);
+    console.log('status: ' + out.status);
+    console.log('stdout: ' + out.stdout.toString('utf8'));
+    console.log('stderr: ' + out.stderr.toString('utf8'));
+    console.log();
 
-out = child_process.spawnSync('false');
-console.log('status: ' + out.status);
-console.log('stdout: ' + out.stdout.toString('utf8'));
-console.log('stderr: ' + out.stderr.toString('utf8'));
-console.log();
+    if(run){
+        out = child_process.spawnSync(run);
+        console.log('status: ' + out.status);
+        console.log('stdout: ' + out.stdout.toString('utf8'));
+        console.log('stderr: ' + out.stderr.toString('utf8'));
+        console.log();
+    }
 
-out = child_process.spawnSync('ls', ['-lhatr']);
-console.log('status: ' + out.status);
-console.log('stdout: ' + out.stdout.toString('utf8'));
-console.log('stderr: ' + out.stderr.toString('utf8'));
-console.log();
+}
+
+module.exports = {
+    test
+}
+    
