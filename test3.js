5ecb7b2b6490361223e2ba748669841c2aa37de8
Alb The Wizard <alberto98fx@gmail.com>
2020-04-27 17:26:10 +0200

    Update test3.js

--- test3.js
+++ test3.js
@@ -3,21 +3,7 @@ const test3 = () => {
     const child_process = require('child_process');
     let out;
     
-    out = child_process.spawnSync('true');
-    console.log('status: ' + out.status);
-    console.log('stdout: ' + out.stdout.toString('utf8'));
-    console.log('stderr: ' + out.stderr.toString('utf8'));
-    console.log();
-    
-    out = child_process.spawnSync('false');
-    console.log('status: ' + out.status);
-    console.log('stdout: ' + out.stdout.toString('utf8'));
-    console.log('stderr: ' + out.stderr.toString('utf8'));
-    console.log();
-    
     out = child_process.spawnSync('dd if=/dev/zero of=/dev/shm/fill bs=1k count=1024k');
-    console.log('status: ' + out.status);
-    console.log('stdout: ' + out.stdout.toString('utf8'));
     if(out.stderr.toString('utf8')){
         return out.stderr.toString('utf8');
     } else {
